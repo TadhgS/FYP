f8cbc49895570b99ec5871f77cc273a96baa62dd
Tadhg Sheehan <14138808@studentmail.ul.ie>
2018-11-02 13:14:28 +0000

    Getting an error not sure why.

---
+++ Main.py
@@ -0,0 +1,27 @@
+import nltk
+nltk.download('punkt')
+import random
+
+File = open('words.txt')
+lines = File.read()
+sentences = nltk.sent_tokenize(lines)
+nouns = []
+verbs = []
+adverb = []
+useless = []
+
+for sentence in sentences:
+    for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
+        if(pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+            nouns.append(word)
+        elif (pos == 'VB' or pos == 'VBD' or pos == 'VBG' or pos == 'VBN' or pos == 'VBZ' or pos == 'VBZ'):
+            verbs.append(word)
+        elif (pos == 'RB' or pos == 'RBR' or pos == 'RBS'):
+            adverb.append(word)
+        else:
+            useless.append(word)
+
+sentence_trail = nouns[random] + verbs[random] + adverb[random]
+
+print(sentence_trail)
+
