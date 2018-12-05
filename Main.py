46156196576d3aade3f69feaf06d88ec7405f876
Tadhg Sheehan <14138808@studentmail.ul.ie>
2018-12-05 18:51:48 +0000

    Program now writes to file, currently limited to only 1000 characters. Still doesn't check quality of sentences.

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import nltk
 import numpy
+import random
 
 File = open('words.txt')
 lines = File.read()
 sentences = nltk.sent_tokenize(lines)
+
 nouns = []
 nounsplural = []
 propernounS = []
@@ -13,6 +15,7 @@ verbpast = []
 verb3person = []
 adverb = []
 useless = []
+punt = []
 
 for sentence in sentences:
     for word, pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
@@ -32,11 +35,28 @@ for sentence in sentences:
             verb3person.append(word)
         elif (pos == 'RB' or pos == 'RBR' or pos == 'RBS'):
             adverb.append(word)
+        elif (pos == 'SYM'):
+            punt.append(word)
         else:
             useless.append(word)
-for x in nouns:
-    {
-print(numpy.random.choice(nouns) + " " + numpy.random.choice(verbpast) + " " + numpy.random.choice(nounsplural))
-    }
+file = open("testfile.txt","w")
 
+for x in range(1,1000):
+    x = random.randint(0,8)
+    if(x == 1):
+        file.write(numpy.random.choice(nouns) + " ")
+    elif(x == 2):
+        file.write(numpy.random.choice(nounsplural) + " ")
+    elif (x == 3):
+        file.write(numpy.random.choice(propernounP) + " ")
+    elif (x == 4):
+        file.write(numpy.random.choice(propernounS) + " ")
+    elif (x == 5):
+        file.write(numpy.random.choice(verbs) + " ")
+    elif (x == 6):
+        file.write(numpy.random.choice(verbpast) + " ")
+    elif (x == 7):
+        file.write(numpy.random.choice(useless) + " ")
+    elif (x == 8):
+        file.write(numpy.random.choice(useless) + " ")
 
