0dac090bf48ee9928202ad804f5d8e538c12d2a9
14138808@studentmail.ul.ie <Liverpoolman1>
2019-02-13 02:28:25 +0000

    Added to scrapping class, found webpage to scrap from

--- scrapping.py
+++ scrapping.py
@@ -1,4 +1,11 @@
 from requests import get
+from urllib.request import urlopen
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+
+url = "https://www.wattpad.com/467372879-silver-moon-chapter-1"
+html = urlopen(url)
+
+bSoup = BeautifulSoup(html, 'lxml')
+type(bSoup)
